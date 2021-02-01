b97de0d368e6e0a629e0ba8cce87e045099ba5af
SimonasMiezetis <76005049+SimonasMiezetis@users.noreply.github.com>
2021-02-01 18:08:12 +0200

    initial JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log('Coming soon');
+import { renderSocials } from './components/renderSocials.js';
+import { socialsData } from './data/socialsData.js';
+
+import { renderProgressBars } from './components/renderProgressBars.js';
+import { progressBarData } from './data/progressBarData.js';
+
+renderProgressBars('.left-column', progressBarData);
+renderSocials('footer > .socials', socialsData);
